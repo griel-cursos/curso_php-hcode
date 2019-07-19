520f00651e705c5132b4b4f7a177a8c92aaccbe7
robzgf <rgrielfilho@gmail.com>
2019-07-18 23:06:23 -0300

    Teste de Conflito da aula de GitHub

--- git/index.php
+++ git/index.php
@@ -11,6 +11,17 @@ echo '<ul>
 		<li>Um arquivo: git add NOME</li>
 		<li>Todos os arquivos: git add --all</li>
 	  </ul>';
+echo 'Confirmar a adição dos arquivos (commit): git commit -m "Mensagem"';
 //Repositorio é um diretorio principal.
-
+echo '<br><br>';
+echo "Preparar arquivos para o site GitHub: git remote add origin LINK DO REPOSITÓRIO";
+echo '<br><br>';
+echo "Enviar arquivos para o site GitHub: git push origin master";
+echo '<ul>
+		<li>git: Comando do Git</li>
+		<li>push: Comando para "empurrar" o arquivo</li>
+		<li>origin: Enviar os arquivos da origem</li>
+		<li>master: Pasta principal no repositório do Git</li>
+	  </ul>';
+echo "Baixar arquivos alterados remotamente: git pull origin master";
 ?>
--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 echo "Meu Primeiro Código PHP. <strong>Olá Mundo.</strong>";
 //Ponto e Vírgula deve ser usado sempre que for encerrar uma instrução.
 
+echo '<br><br>';
+
+echo "Teste de Conflitos";
+//Linha adicionada para a aula de conflitos do GitHub
+
 //Tag de Encerramento de PHP
 ?>
 
