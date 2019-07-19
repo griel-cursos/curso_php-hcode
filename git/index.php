297c27d25cc81b4ed90ed18aed866bdf58711639
robzgf <rgrielfilho@gmail.com>
2019-07-18 22:34:42 -0300

    Criei meu repositorio

---
+++ git/index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Clonar repositorio Git: git clone LINK";
+echo '<br><br>';
+echo "Clonar repositório Git com nome: git clone LINK NOME";
+echo '<br><br>';
+echo "Criar repositório (comando para executar na pasta): git init";
+echo '<br><br>';
+echo "Adicionar arquivo ao git:";
+echo '<ul>
+		<li>Um arquivo: git add NOME</li>
+		<li>Todos os arquivos: git add --all</li>
+	  </ul>';
+//Repositorio é um diretorio principal.
+
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//Tag de Abertura do PHP
+
+echo "Meu Primeiro Código PHP. <strong>Hello World.</strong>";
+//Ponto e Vírgula deve ser usado sempre que for encerrar uma instrução.
+
+//Tag de Encerramento de PHP
+?>
+
