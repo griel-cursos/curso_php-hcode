e9a211be28f28c8295b437f7984a0302071b6858
robzgf <rgrielfilho@gmail.com>
2019-07-21 20:40:50 -0300

    Adicionado Operadores ao índice

--- indice.php
+++ indice.php
@@ -27,9 +27,37 @@
 				<li>
 					<a href="variaveis/exemplo-05.php">Exemplos de Escopo de Variáveis, Criação de Funções</a>
 				</li>
+				<li>
+					<a href="operadores/exemplo-06.php">Verificador de Variáveis do PHP 7</a>
+				</li>
 			</ul>
-		
-		
+			
+			<h1>Operadores</h1>
+			
+			<ul>
+				<li>
+					<a href="operadores/exemplo-01.php">Início de Operadores, Concatenar e Adicionar informações nas Variáveis</a>
+				</li>
+				<li>
+					<a href="operadores/exemplo-02.php">Operadores de Atribuição</a>
+				</li>
+				<li>
+					<a href="operadores/exemplo-03.php">Operadores Aritiméticos, Matemáticos</a>
+				</li>
+				<li>
+					<a href="operadores/exemplo-04.php">Verificações Matemáticas, se é maior, igual, menor</a>
+				</li>
+				<li>
+					<a href="operadores/exemplo-05.php">Operadores e Verificadores do PHP 7</a>
+				</li>
+				<li>
+					<a href="operadores/exemplo-07.php">Operadores Incrementais e Decrementais</a>
+				</li>
+				<li>
+					<a href="operadores/exemplo-08.php">Ordem de Execução dos Operadores, Precedencia de Operador</a>
+				</li>
+			</ul>
+					
 		</body>
 
 	</head>
