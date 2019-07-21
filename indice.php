04a1bb89d352d0a2e83f285203572a85be4cae9c
robzgf <rgrielfilho@gmail.com>
2019-07-21 19:33:52 -0300

    Criei um índice para consultas futuras. Exemplo, em qual aula foi ensinado tal função.

---
+++ indice.php
@@ -0,0 +1,33 @@
+<html>
+	<head>
+		
+		<body>
+			
+			<h1>Comandos do Git</h1>
+			<ul>
+				<li>
+					<a href="git/index.php">Lista de Comandos do Git</a>
+				</li>
+			</ul>
+			
+			<h1>Variáveis</h1>
+			<ul>
+				<li>
+					<a href="variaveis/exemplo-01.php">Exemplos Básicos de Variáveis</a>
+				</li>
+				<li>
+					<a href="variaveis/exemplo-02.php">Concatenar, destruir variável, função de verificar variavel (isset)</a>
+				</li>
+				<li>
+					<a href="variaveis/exemplo-03.php">Início de Arrays, Variáveis vazias e nulas</a>
+				</li>
+				<li>
+					<a href="variaveis/exemplo-04.php">Manipular URL, Retornar IP e Retornar Nome do Arquivo</a>
+				</li>
+			</ul>
+		
+		
+		</body>
+
+	</head>
+</html>
