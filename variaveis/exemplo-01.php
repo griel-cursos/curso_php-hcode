4f17584c3eff5b52d5bd6b297654b9dc59265b7a
robzgf <rgrielfilho@gmail.com>
2019-07-21 19:49:52 -0300

    Exemplo básico de variáveis

---
+++ variaveis/exemplo-01.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = "Roberto";
+//Variaveis são espaços na memoria do servidor onde vocÊ irá guardar informação temporariamente. O nome é variavel pois podem ser alterados.
+
+echo $nome;
+
+echo '<br><br>';
+
+var_dump($nome);
+
+
+?>
