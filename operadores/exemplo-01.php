baede51101d78fa295057f394b1eb53130ee3b8c
robzgf <rgrielfilho@gmail.com>
2019-07-21 20:41:06 -0300

    Início de Operadores, Concatenar e Adicionar informações nas Variáveis

---
+++ operadores/exemplo-01.php
@@ -0,0 +1,14 @@
+<?php
+
+$Nome = "Roberto";
+//	  ^-Operador
+
+echo $Nome . " continuação de texto <br>";
+//		   ^-Operadores de String
+
+$Nome .= " Estudando";
+//    ^- Adicionou uma informação à uma variável
+
+echo $Nome;
+
+?>
