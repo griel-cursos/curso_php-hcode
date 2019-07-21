6f77f8b700461d0f596f56cbb24c097c19d86e65
robzgf <rgrielfilho@gmail.com>
2019-07-21 20:41:26 -0300

    Operadores Aritiméticos, Matemáticos

---
+++ operadores/exemplo-03.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 10;
+$b = 2;
+
+echo 'Soma: <br>';
+echo $a + $b;
+echo '<br><br>';
+
+echo 'Subtração: <br>';
+echo $a - $b;
+echo '<br><br>';
+
+echo 'Multiplicação: <br>';
+echo $a * $b;
+echo '<br><br>';
+
+echo 'Divisão: <br>';
+echo $a / $b;
+echo '<br><br>';
+
+echo 'Módulo (Resto da Divisão) <br>';
+echo $a % $b;
+echo '<br><br>';
+
+echo 'Exponenciação (elevado a...) <br>';
+echo $a ** $b;
+?>
