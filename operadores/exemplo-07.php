f2048452b88d9c9bff4b0fab6168dc3a76fc8cb0
robzgf <rgrielfilho@gmail.com>
2019-07-21 20:42:05 -0300

    Operadores Incrementais e Decrementais

---
+++ operadores/exemplo-07.php
@@ -0,0 +1,22 @@
+<?php
+
+$a = 10;
+//$a inicialmente vale 10
+
+echo $a++;
+//Exibe o valor de $a, e depois somou mais 1
+
+echo '<br>';
+
+echo $a;
+
+echo '<br>';
+
+echo ++$a;
+//Já exibe o valor de $a com a soma
+
+echo '<br>';
+
+echo --$a;
+//Já exibe o valor de $a com a subtração
+?>
