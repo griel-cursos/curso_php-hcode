2aa39fd6b081abe312a01ef52903241ad049e5de
robzgf <rgrielfilho@gmail.com>
2019-07-21 21:16:45 -0300

    Diferença de Aspas Simples e Aspas Duplas

---
+++ strings/exemplo-01.php
@@ -0,0 +1,20 @@
+<?php
+
+$Nome = "Roberto";
+
+$Nome2 = 'Griel Filho';
+
+var_dump($Nome, $Nome2);
+
+echo '<br><br>';
+
+echo "ABC $Nome";
+//Aspas Duplas faz INTERPOLAÇÃO DE VARIÁVEIS. Permite variaveis dentro da String
+
+echo '<br><br>';
+
+echo 'ABC $Nome';
+//Aspas Simples transforma tudo em texto
+
+
+?>
