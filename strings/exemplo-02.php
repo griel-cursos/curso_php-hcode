1d6f157a684d7ae3651fd84cf9921723d4acc9a8
robzgf <rgrielfilho@gmail.com>
2019-07-21 21:16:54 -0300

    Funções e Recursos de Strings, Deixar Maiusculo, Minusculo

---
+++ strings/exemplo-02.php
@@ -0,0 +1,34 @@
+<?php
+
+$Nome = "roberto griel filho";
+
+echo $Nome;
+
+echo '<br><br>';
+
+echo "Deixar o texto maiusculo: ";
+
+echo strtoupper($Nome);
+
+echo '<br><br>';
+
+echo "Deixar o texto em minúsculo: ";
+
+echo strtolower($Nome);
+
+echo '<br><br>';
+
+echo "Deixar a Primeira Letra de Cada Palavra Maiuscula: ";
+
+echo ucwords($Nome);
+
+echo '<br><br>';
+
+echo "Deixar a primeira letra maiuscula: ";
+
+echo ucfirst($Nome);
+
+
+
+
+?>
