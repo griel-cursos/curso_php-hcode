40514b80176aff8e262a00d7b650799dafbdae13
robzgf <rgrielfilho@gmail.com>
2019-07-21 21:17:02 -0300

    Funções e Recursos de Strings, Substituir Textos

---
+++ strings/exemplo-03.php
@@ -0,0 +1,12 @@
+<?php
+
+$Empresa = "Griel";
+
+$Empresa = str_replace("i", "1", $Empresa);
+//Variavel   Função    Trocar I pelo 1    Nome da Variável
+$Empresa = str_replace("e", "3", $Empresa);
+//Variavel   Função    Trocar e pelo 3    Nome da Variável
+
+echo $Empresa;
+
+?>
