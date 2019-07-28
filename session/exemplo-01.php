445aa41160c4b5abd247c136db2421867517205d
robzgf <rgrielfilho@gmail.com>
2019-07-28 14:50:10 -0300

    Abertura de Sessão e Criação de Sessão

---
+++ session/exemplo-01.php
@@ -0,0 +1,12 @@
+<?php
+
+//session_start();
+//Sessão iniciada nesta página
+
+require_once("config.php");
+//Chamando o arquivo de configuração
+
+$_SESSION["nome"] = "Roberto";
+//Criei uma sessão "nome" que receberá o valor "Roberto"
+
+?>
