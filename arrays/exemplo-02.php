7383e8e495475b0034e68176548b664a9296d8f6
robzgf <rgrielfilho@gmail.com>
2019-07-28 12:30:30 -0300

    Revisão da segunda aula de Array

--- arrays/exemplo-02.php
+++ arrays/exemplo-02.php
@@ -2,6 +2,7 @@
 
 //Exemplos de Arrays Bi-Dimensionais
 
+//Primeiro colchete é o índice; Segundo colchete é a posição daquele índice
 $Carros [0][0] = "GM";
 $Carros [0][1] = "Cobalt";
 $Carros [0][2] = "Onix";
@@ -13,13 +14,21 @@ $Carros [1][1] = "Fiesta";
 $Carros [1][2] = "Fusion";
 $Carros [1][3] = "Ecosport";
 
-echo $Carros[0][3];
+echo 'Exibe o array no índice 0, posição 3: ' . $Carros[0][3];
 //Exibe da array $Carros o que tem na posição 3 do índice 0
 
 echo '<br>';
 
-echo end($Carros[1]);
+echo 'Exibe o array no índice 1, posição 3: ' . $Carros[1][3];
+
+echo '<br>';
+
+echo 'Exibe a última informação do Array no índice 1: ' . end($Carros[1]);
 //Exibe a ultima informação do indice 1
+
+echo '<br>';
+
+
 
 
 ?>
