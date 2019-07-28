b90680e633053ca1242ebd183d109995ebd753f1
robzgf <rgrielfilho@gmail.com>
2019-07-28 13:25:41 -0300

    Exemplo de criação e explicação de constante

---
+++ constante/exemplo-01.php
@@ -0,0 +1,19 @@
+<?php
+
+define("SERVIDOR", "127.0.0.1");
+// Nome da Constante  Valor da Constante
+
+echo SERVIDOR;
+//Irá exibir o valor da constante SERVIDOR.
+
+/////////////////
+
+echo '<BR><BR>';
+
+define("NOME", "Roberto");
+define("IDADE", "30");
+
+echo 'O nome é ' . NOME . ' e a idade é ' . IDADE;
+
+
+?>
