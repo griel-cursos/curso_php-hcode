d266a3458784ecc383831e1c13564efac741bb7d
robzgf <rgrielfilho@gmail.com>
2019-07-28 13:30:32 -0300

    Criação de Constante com Array (Novidade do PHP 7)

---
+++ constante/exemplo-02.php
@@ -0,0 +1,16 @@
+<?php
+
+define("BANCO_DE_DADOS", [
+	'127.0.0.1',
+	'root',
+	'password',
+	'test'
+]);
+//Constante com dados em Array. Primeiro o nome da constante, em seguida em [] os dados do Array
+
+print_r(BANCO_DE_DADOS);
+
+echo '<br><br>';
+
+
+?>
