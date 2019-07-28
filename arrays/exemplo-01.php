3adb198886949b4ac5d3ec03c510bc23bf95af87
robzgf <rgrielfilho@gmail.com>
2019-07-28 12:26:32 -0300

    Revisão da Aula de Array

--- arrays/exemplo-01.php
+++ arrays/exemplo-01.php
@@ -1,7 +1,16 @@
 <?php
 
+//Exemplo de array de Frutas
+
 $Frutas = array("Laranja", "Abacaxi", "Melancia");
-//Todo Array inicia em zero
+//Variavel  Função (Dados que irá dentro do Array, separados por virgula, exceto o último)
+
+/*
+
+Primeira Regra: Todo Array começa em zero
+Segunda Regra: Array com somente uma dimensão é chamado de vetor. Neste exemplo é um vetor.
+
+*/
 
 print_r($Frutas);
 
