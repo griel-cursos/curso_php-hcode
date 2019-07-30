540892879f3e936fec696fa2804f3331b26cf2b7
robzgf <rgrielfilho@gmail.com>
2019-07-30 19:25:37 -0300

    Criando Objeto a Partir de uma Classe, Exibindo Data Através de Classe

---
+++ date/exemplo-04.php
@@ -0,0 +1,8 @@
+<?php
+
+$dt = new DateTime();
+		//Criando Objeto a partir de uma Classe()
+
+echo $dt->format("d/m/Y H:i:s");
+	//dt é uma Instancia da Classe. Para acessar o método usa o comando ->
+?>
