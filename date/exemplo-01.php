e509f3a457b643ef1779db27be89905f25d55915
robzgf <rgrielfilho@gmail.com>
2019-07-30 19:25:18 -0300

    Exibir Datas, Exibir o Timestamp, Mais Opções de Datas

---
+++ date/exemplo-01.php
@@ -0,0 +1,14 @@
+<?php
+
+echo date("d/n/Y H:i:s");
+
+echo '<br>';
+
+echo 'Mais opções para exibir data e horas: <a href=https://www.php.net/manual/pt_BR/function.date.php>Manual PHP</a>';
+
+echo '<br>';
+
+echo 'Quantidades de segundo desde 01/01/1970: '. time();
+
+
+?>
