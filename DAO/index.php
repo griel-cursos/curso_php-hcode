4f1a7db089482bb9906c45774232d09517ba840c
rgrielfilho@gmail.com <$%Nayane17()>
2019-08-12 17:52:51 -0300

    Adicionado comando para visualizar dados

--- DAO/index.php
+++ DAO/index.php
@@ -2,10 +2,13 @@
 
 require_once("config.php");
 
-$sql = new SQL();
+/* $sql = new SQL();
 
 $usuarios = $sql->select("select * from tb_usuarios");
 
-echo json_encode($usuarios);
+echo json_encode($usuarios);*/
 
+$root = new Usuario();
+$root->loadById(4);
+echo $root;
 ?>
