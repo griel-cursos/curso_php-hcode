32e772bf06eedae18dc0b526d9c98f7932b2854e
rgrielfilho@gmail.com <$%Nayane17()>
2019-08-13 19:01:03 -0300

    Criando e Apagando Pastas com Função do PHP

--- DIR/exemplo-01.php
+++ DIR/exemplo-01.php
@@ -1,16 +1,20 @@
 <?php
 
 $name = "images";
+//Nome do diretório que quero criar
 
 if (!is_dir($name)) {
 	
 	mkdir($name);
+	//Comando para Criar uma Pasta
 	
 	echo "Diretório $name criado com sucesso.";
 	
 } else {
 	
 	rmdir($name);
+	//Comando para Apagar uma Pasta
+	
 	echo "O diretório $name já existe mas acabou de ser removido";
 	
 }
