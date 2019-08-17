7f9f8bdb0628a6f4725b8146ff796a84a056e31c
robzgf <rgrielfilho@gmail.com>
2019-08-16 23:45:25 -0300

    Função para Gerenciar e Tratar Erros

--- erros/exemplo-01.php
+++ erros/exemplo-01.php
@@ -1,5 +1,6 @@
 <?php
 
+//Função que irá trabalhar com os erros
 function error_handler($code, $message, $file, $line) {
 	
 	echo json_encode(array(
@@ -10,9 +11,10 @@ function error_handler($code, $message, $file, $line) {
 	));
 	
 }
-
+//Define que o erros serão gerenciados pela minha função
 set_error_handler("error_handler");
 
+//exemplo básico de erro de divisão por 0
 echo 100/0;
 
 ?>
