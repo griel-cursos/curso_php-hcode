4e4529b6416bd30645d4812743904d0c9411430b
robzgf <rgrielfilho@gmail.com>
2019-08-18 19:41:16 -0300

    Separando o Código PHP

---
+++ tpl/index.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once("vendor/autoload.php");
+
+// namespace
+use Rain\Tpl;
+// config
+$config = array(
+    "tpl_dir"       => "tpl/",
+    "cache_dir"     => "cache/",
+    "debug"         => false, // set to false to improve the speed
+);
+Tpl::configure( $config );
+
+// create the Tpl object
+$tpl = new Tpl;
+// assign a variable
+$tpl->assign( "name", "Roberto" );
+$tpl->assign( "version", PHP_VERSION );
+// assign an array
+//$tpl->assign( "week", array( "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ) );
+// draw the template
+
+
+
+$tpl->draw( "index" );
+
+?>
