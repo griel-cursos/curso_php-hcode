28d4bf4297eb730de628ad8029c3c5c3a7aeeda1
robzgf <rgrielfilho@gmail.com>
2019-08-20 13:09:15 -0300

    Arquivos de Exemplo com Slim Framework

--- website/index.php
+++ website/index.php
@@ -1,5 +1,18 @@
 <?php
 
-phpinfo();
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->get('/', function() {
+	echo json_encode(array(
+		"date"=>date("Y/m/d")
+	));
+});
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+$app->run();
 
 ?>
