96696750918d0d530879f0df40528ff31c87b746
robzgf <rgrielfilho@gmail.com>
2019-08-24 22:10:28 -0300

    Revisão da Aula de Composer

---
+++ Composer/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+?>
+
+<h1>Usando o Composer</h1>
+
+<br><br>
+
+<a href="https://getcomposer.org/download/">Baixar Composer</a>
+
+<br><br>
+
+<p>Iniciar o Composer: Na pasta onde irá usar, botão direito, GIT Bash Here, <b>composer init</b></p>
+<p>Após configurar o json, comando para baixar as dependências: <b>composer update</b></p>
+<p>Regras de Boas Práticas do PHP: <b><a href="https://www.php-fig.org/">PHP-Fig</a></b></p>
